64fcf3405110f6edd3f171489e97b2ded163e4a8
Gaston Perez <gastonperez-97@hotmail.com>
2022-02-28 18:31:29 -0300

    moved img folder to src

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
 
+import logo from "../../assets/img/logo.jpg";
+
 import "./Navbar.css";
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
-			<img className="navbar-logo" src="./img/logo.jpg" alt="FeLiNa logo" />
+			<img className="navbar-logo" src={ logo } alt="FeLiNa logo" />
 
 			<div className="nav-btn-container">
 				<NavLink to="/">Home</NavLink>
