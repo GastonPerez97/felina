2e5b448378bd945aa3aaf939f8500656df2d6b03
Gaston Perez <gastonperez-97@hotmail.com>
2022-03-05 17:29:55 -0300

    added nanoid dependency. refactored breaking bad api service. added content in episode component

--- src/components/Episodes/Episodes.js
+++ src/components/Episodes/Episodes.js
@@ -1,12 +1,118 @@
-import { Heading } from "@chakra-ui/react";
+import { useEffect, useState, useMemo } from "react";
+import {
+	Heading,
+	Stack,
+	Skeleton,
+	Tag,
+	Tabs,
+	Tab,
+	TabList,
+	TabPanels,
+	TabPanel,
+	Table,
+	Thead,
+	Tbody,
+	Tr,
+	Th,
+	Td,
+} from "@chakra-ui/react";
 
-import "./Episodes.css";
+import { getAllEpisodes } from "../../services/breaking-bad-api";
+import { nanoid } from "nanoid";
 
 const Episodes = () => {
+	const [episodes, setEpisodes] = useState([]);
+
+	useEffect(() => {
+		getAllEpisodes().then(episodes => {
+			setEpisodes(episodes);
+		});
+	}, []);
+
+	const loadingSkeleton = useMemo(() => {
+		return (
+			<>
+				<Skeleton height="41px" />
+
+				<Stack px={3} py={1}>
+					<Skeleton height="40px" />
+
+					<Stack p={1}>
+						{Array(10)
+							.fill(1)
+							.map(item => (
+								<Skeleton key={nanoid()} h="70px" />
+							))}
+					</Stack>
+				</Stack>
+			</>
+		);
+	}, []);
+
+	const getEpisodeElementsBySeason = seasonNumber => {
+		return episodes
+			.filter(episode => episode.season === seasonNumber)
+			.map(episode => {
+				return (
+					<Tr key={nanoid()}>
+						<Td w={"10%"}>{episode.episode}</Td>
+						<Td w={"20%"}>{episode.title}</Td>
+						<Td w={"20%"}>{episode.air_date}</Td>
+						<Td w={"50%"}>
+							{episode.characters.map(character => (
+								<Tag m={1} key={nanoid()}>
+									{character}
+								</Tag>
+							))}
+						</Td>
+					</Tr>
+				);
+			});
+	};
+
+	const getTableElementBySeason = seasonNumber => {
+		return (
+			<Table variant="simple">
+				<Thead>
+					<Tr>
+						<Th>#</Th>
+						<Th>Title</Th>
+						<Th>Air Date</Th>
+						<Th>Characters</Th>
+					</Tr>
+				</Thead>
+				<Tbody>{getEpisodeElementsBySeason(seasonNumber)}</Tbody>
+			</Table>
+		);
+	};
+
 	return (
-		<Heading fontSize="45px" mb={10} textAlign="center">
-			EPISODES
-		</Heading>
+		<>
+			<Heading fontSize="45px" textAlign="center" mb={8}>
+				EPISODES
+			</Heading>
+
+			{episodes.length === 0 ? (
+				<Stack>{loadingSkeleton}</Stack>
+			) : (
+				<Tabs variant="enclosed" isFitted>
+					<TabList>
+						<Tab>Season 1 (2008)</Tab>
+						<Tab>Season 2 (2009)</Tab>
+						<Tab>Season 3 (2010)</Tab>
+						<Tab>Season 4 (2011)</Tab>
+						<Tab>Season 5 (2012 - 2013)</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>{getTableElementBySeason("1")}</TabPanel>
+						<TabPanel>{getTableElementBySeason("2")}</TabPanel>
+						<TabPanel>{getTableElementBySeason("3")}</TabPanel>
+						<TabPanel>{getTableElementBySeason("4")}</TabPanel>
+						<TabPanel>{getTableElementBySeason("5")}</TabPanel>
+					</TabPanels>
+				</Tabs>
+			)}
+		</>
 	);
 };
 
--- src/services/breaking-bad-api.js
+++ src/services/breaking-bad-api.js
@@ -1,9 +1,13 @@
 const baseUrl = "https://www.breakingbadapi.com/api";
 
-const getAllCharacters = () => {
-	return fetch(baseUrl + "/characters")
+const fetchData = endpoint => {
+	return fetch(baseUrl + endpoint)
 		.then(res => res.json())
-		.then(characters => characters);
+		.then(data => data);
 };
 
-export { getAllCharacters };
+const getAllCharacters = () => fetchData("/characters?category=Breaking+Bad");
+const getAllEpisodes = () => fetchData("/episodes?series=Breaking+Bad");
+const getAllQuotes = () => fetchData("/quotes?series=Breaking+Bad");
+
+export { getAllCharacters, getAllEpisodes, getAllQuotes };
